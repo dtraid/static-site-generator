189f75203b7891e97bca9496273953e46a0f4bab
Diego Traíd <diego@revieve.com>
2026-01-04 12:33:25 -0500

    Update

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,8 +1,20 @@
 import os
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+
+
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -18,6 +30,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -32,19 +46,3 @@ def extract_title(md):
         if line.startswith("# "):
             return line[2:]
     raise ValueError("no title found")
-
-def generate_page_recursive(source_dir_path, template_path, dest_dir_path):
-    if not os.path.exists(dest_dir_path):
-        os.mkdir(dest_dir_path)
-
-    for filename in os.listdir(source_dir_path):
-        from_path = os.path.join(source_dir_path, filename)
-        dest_path = os.path.join(dest_dir_path, filename)
-
-        if os.path.isfile(from_path):
-            root, ext = os.path.splitext(filename)
-            if ext == '.md':
-                print(f" * {from_path} -> {dest_path}")
-                generate_page(from_path, template_path, os.path.join(dest_dir_path, f"{root}.html"))
-        else:
-            generate_page_recursive(from_path, template_path, dest_path)
